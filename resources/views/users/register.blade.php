81dc88935da686fea08166b95e2ed557380cde9d
Piusshungu <shungupius@gmail.com>
2020-10-22 01:40:23 +0300

    Disabled javascripts

--- resources/views/users/register.blade.php
+++ resources/views/users/register.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>The Easiest Way to Add Input Masks to Your Forms</title>
+    <title>User Registration</title>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/simple-line-icons/2.4.1/css/simple-line-icons.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/css/style.css">
@@ -133,18 +133,20 @@
             </div>
             <div class="form-group">
                 <button type="button" class="btn btn-block create-account">Create Account</button>
+              
             </div>
+            <a href="#" class="forgot">Do you have an account? Login</a>
         </form>
         
     </div>
     <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.15/jquery.mask.min.js"></script>
     <script src="assets/js/script.js"></script>
-    <script>
+    <!-- <script>
         $(document).ready(function(){
   $('#birth-date').mask('00/00/0000');
   $('#phone-number').mask('0000-0000');
  })
-    </script>
+    </script> -->
 </body>
 </html>
